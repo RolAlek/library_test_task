7abd568c7a7ba3684f3e6427816ca236f765c9f9
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-07-28 00:03:26 +0900

    add users models

--- library_project/user/models.py
+++ library_project/user/models.py
@@ -1,3 +1,25 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    LIBRARIAN = 'librarian'
+    READER ='reader'
+    ROLES = [
+        (LIBRARIAN, 'Библиотекарь'),
+        (READER, 'Читатель'),
+    ]
+    role = models.CharField(choices=ROLES)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Librarian(CustomUser):
+    staff_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+
+class Reader(CustomUser):
+    address = models.CharField(max_length=100)
