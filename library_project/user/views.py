a238706ba94632a8c44a222eb998f0c461896d9c
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-07-28 20:17:57 +0900

    add user registration feature

--- library_project/user/views.py
+++ library_project/user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+User = get_user_model()
+
+
+class CreateCustomUserView(CreateView):
+    model = User
+    form_class = CustomUserCreationForm
+    template_name = "registration/registration_form.html"
+    success_url = reverse_lazy("homepage")
